7dde74a0105c9ca714566352bc07ad8311807229
sdossettswift <sdossettswift@gmail.com>
2016-05-31 12:12:43 -0500

    sings 99 bottles of beer

--- say-hi.rb
+++ say-hi.rb
@@ -1,4 +1,6 @@
-
+starting_number =100
 99.times do
-  puts "Hello World!"
+  starting_number = starting_number - 1
+  system("say '#{starting_number} Bottles of beer on the wall!'")
+  system("say 'Take one down, pass it around'")
 end
